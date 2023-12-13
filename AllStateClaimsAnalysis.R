dc23f772c2ff141e38f2cb5ba7e5a57b8460cc04
eaa54 <144567587+eaa54@users.noreply.github.com>
2023-12-13 12:11:57 -0700

    Final Allstate Code with 3 models

--- AllStateClaimsAnalysis.R
+++ AllStateClaimsAnalysis.R
@@ -2,65 +2,170 @@ library(vroom)
 library(tidyverse)
 library(tidymodels)
 library(DataExplorer)
+library(embed)
+library(bonsai)
+library(lightgbm)
 
 train <- vroom("C:/Users/eaa54/Documents/School/STAT348/AllState/train.csv")
 test <- vroom("C:/Users/eaa54/Documents/School/STAT348/AllState/test.csv")
 
-hist(train$loss) #right skewed
-plot_correlation(train, type = "continuous") #high correlation off the diagonal
+#exploratory data analysis
+hist(train$loss) #response heavily right skewed
+plot_correlation(train, type = "continuous") #some high correlation off the diagonal
 
-train$loss <- (train$loss + 1)*.25 #based on suggestion from competition winner
+train$loss <- (train$loss + 1)^.25 #based on suggestion from competition winner
+hist(train$loss) #makes response more normal, but keeps slight right skew
 
+#create recipe
 allstate_recipe <- recipe(loss ~ ., train) %>%
-  update_role(id, new_role = "ID") %>%
-  step_scale(all_numeric_predictors()) %>%
-  step_corr(all_numeric_predictors(), threshold = 0.6) %>%
-  step_novel(all_nominal_predictors()) %>%
-  step_unknown(all_nominal_predictors()) %>%
-  step_dummy(all_nominal_predictors())
+  step_lencode_mixed(all_nominal_predictors(), outcome = vars(loss))
 
-##Neural Net##
-nn_model <- mlp(hidden_units = tune(),
-                epochs = 50) %>% #or 100 or 250
-  set_engine("nnet") %>% 
-  set_mode("classification")
+#################################
+##PENALIZED LOGISTIC REGRESSION##
+#################################
+#create model
+preg_model <- linear_reg(penalty=tune(), mixture=0) %>%
+  set_engine("glmnet")
 
-# set workflow
-nn_wf <- workflow() %>%
+preg_wf <- workflow() %>%
   add_recipe(allstate_recipe) %>%
-  add_model(nn_model)
+  add_model(preg_model)
 
-nn_tuneGrid <- grid_regular(hidden_units(range=c(1, 75)),
-                            levels=3)
+#set workflow
+preg_wf <- workflow() %>%
+  add_recipe(allstate_recipe) %>% #add recipe
+  add_model(preg_model) #add model
 
-# Set up k-fold cross validation and run it
-nn_folds <- vfold_cv(train, v = 5, repeats = 1)
+#set up grid of tuning values
+tuning_grid <- grid_regular(penalty(),
+                            levels = 5) #levels^2 total tuning possibilities
 
-CV_nn_results <- nn_wf %>%
-  tune_grid(resamples = nn_folds,
-            grid = nn_tuneGrid,
-            metrics = metric_set(accuracy))
+#set up cv
+folds <- vfold_cv(train, v = 5, repeats = 1)
 
-CV_nn_results %>% 
-  collect_metrics() %>% 
-  filter(.metric=="accuracy") %>%
-  ggplot(aes(x=hidden_units, y=mean)) + geom_line()
 
-# Find Best Tuning Parameters
-bestTune_nn <- CV_nn_results %>%
-  select_best("accuracy")
+CV_results <- preg_wf %>%
+  tune_grid(resamples = folds,
+            grid = tuning_grid,
+            metrics = metric_set(mae)) #mean absolute error used in this kaggle comp
+
+#find Best Tuning Parameters
+bestTune <- CV_results %>%
+  select_best("mae")
+
+#finalize the workflow & fit it
+final_wf <- preg_wf %>%
+  finalize_workflow(bestTune) %>%
+  fit(data = train)
+
+rf_predictions <- final_wf %>%
+  predict(new_data = test)
+
+#format for Kaggle
+rf_final <- rf_predictions %>%
+  bind_cols(test) %>%
+  select(id, .pred) %>%
+  rename(loss = .pred) %>%
+  mutate(loss = (loss)^4-1)
+
+write_csv(rf_final, "PRegSubmission.csv")
+
+#SCORE:1215.35
+
+############
+##BOOSTING##
+############
+#create model
+boost_model <- boost_tree(tree_depth=tune(),
+                          trees=tune(),
+                          learn_rate=tune(),
+                          mode = "regression") %>%
+               set_engine("lightgbm") #or "xgboost" but lightgbm is faster
+
+#set workflow
+boost_wf <- workflow() %>%
+  add_recipe(allstate_recipe) %>%
+  add_model(boost_model)
+
+#set up tuning grid
+boost_tuneGrid <- grid_regular(tree_depth(), trees(), learn_rate(), levels = 3)
+
+#set up cv
+boost_folds <- vfold_cv(train, v = 5, repeats = 1)
+
+CV_boost_results <- boost_wf %>%
+  tune_grid(resamples = boost_folds,
+            grid = boost_tuneGrid,
+            metrics = metric_set(mae))
+
+#find best tuning parameters
+bestTune_boost <- CV_boost_results %>%
+  select_best("mae") #mean absolute error used by this particular Kaggle comp
 
 #finalize workflow and fit it
-final_nn_wf <- nn_wf %>%
-  finalize_workflow(bestTune_nn) %>%
+final_boost_wf <- boost_wf %>%
+  finalize_workflow(bestTune_boost) %>%
   fit(train)
 
-pred_nn <- predict(final_nn_wf, new_data = test, type = "class") %>%
-  bind_cols(., test) %>%
-  rename(type = .pred_class) %>%
-  select(id, type)
+#format for kaggle
+pred_boost <- predict(final_boost_wf, new_data = test) 
+boost_final <- pred_boost %>%
+  bind_cols(test) %>%
+  select(id,.pred) %>%
+  rename(loss = .pred) %>%
+  mutate(loss = loss^4-1)
 
-vroom_write(pred_nn, "GGG_preds_nn.csv", delim = ",")
+write_csv(boost_final, "boostSubmission.csv")
 
-  
+##SCORE: 1,123.17
 
+#################
+##RANDOM FOREST##
+#################
+#set up the model
+my_mod <- rand_forest(mtry = 5,
+                      min_n = 2,
+                      trees = 1000) %>%
+          set_engine("ranger") %>%
+          set_mode("regression") #parameters chosen based on classmate's CV (took 20+ hours to run, so for simplicity I used his parameters)
+
+#set up workflow
+rf_wf <- workflow() %>%
+  add_recipe(rf_recipe) %>%
+  add_model(my_mod) %>%
+  fit(data = train)
+
+#set up tuning grid - this is how I would tune it
+# tuning_grid <- grid_regular(mtry(c(1,5)),
+#                             min_n(),
+#                             levels = 5) #levels^2 total tuning possibilities
+#
+# folds <- vfold_cv(train, v = 3, repeats=1)
+#
+#run the cv
+# CV_results <- rf_workflow %>%
+#   tune_grid(resamples = folds,
+#             grid = tuning_grid,
+#                  metrics = metric_set(mae))
+#
+#find Best Tuning Parameters
+# bestTune <- CV_results %>%
+#   select_best("mae")
+#
+#finalize the Workflow & fit it
+# final_wf <- rf_workflow %>%
+#   finalize_workflow(bestTune) %>%
+#   fit(data = train)
+
+rf_predictions <- rf_wf %>%
+  predict(new_data = test)
+
+rf_final <- rf_predictions %>%
+  bind_cols(test) %>%
+  select(id,.pred) %>%
+  rename(loss = .pred) %>%
+  mutate(loss = loss^4-1)
+
+write_csv(rf_final, "RFSubmission.csv")
+
+##SCORE: 1,186.36
